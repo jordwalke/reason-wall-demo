54d885d41323987951e0201d4807f9ec6418bab7
Jordan W <jordojw@gmail.com>
2018-02-19 11:54:10 -0800

    Test both preview and latest releases.

--- travisTest.sh
+++ travisTest.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
-
+npm remove -g esy
+# Test esy preview branch - make the release with it.
+npm install -g esy@preview
 esy install
 esy build
 esy x which ReasonWallGallery.exe
@@ -11,3 +13,12 @@ npm install -g ./_release
 which ReasonWallGallery.exe
 which ReasonWallUi.exe
 npm remove -g reason-wall-demo
+
+# Now test the latest branch of esy
+npm remove -g esy
+npm install -g esy@latest
+esy install
+esy build
+esy x which ReasonWallGallery.exe
+esy x which ReasonWallUi.exe
+
