4667984ed310fd92d5e263fd65ac1d08c7197e8d
Jordan Walke <jordojw@gmail.com>
2018-02-19 00:11:25 -0800

    adjust travis test

--- travisTest.sh
+++ travisTest.sh
@@ -6,12 +6,7 @@ esy build
 esy x which ReasonWallGallery.exe
 esy x which ReasonWallUi.exe
 esy release bin
-cd _release
-cd *
-npm pack
-mv reason-wall-demo-* ../../
-cd ../../
-npm install -g ./reason-wall-demo-*
+npm install -g ./_release
 # Now we don't need the esy x prefix
 which ReasonWallGallery.exe
 which ReasonWallUi.exe
