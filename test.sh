2cef159799ccd5b4fbe33cb7c7c1aea70130ab14
Jordan Walke <jordojw@gmail.com>
2018-02-18 23:55:15 -0800

    update travis

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@ set -e
 
 esy install
 esy build
+esy x which ReasonWallGallery.exe
+esy x which ReasonWallUi.exe
 esy release bin
 cd _release
 cd *
@@ -10,5 +12,7 @@ npm pack
 mv reason-wall-demo-* ../../
 cd ../../
 npm install -g ./reason-wall-demo-*
-which ReasonWallDemo.exe
+# Now we don't need the esy x prefix
+which ReasonWallGallery.exe
+which ReasonWallUi.exe
 npm remove -g reason-wall-demo
